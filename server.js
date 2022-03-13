a74008eb0ec07177c2718c97fd2644bacf0d1616
anakinsonone <akkhi.sonone@gmail.com>
2022-03-13 11:50:02 +0530

    second commit

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 const { join } = require('path');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const { profile } = require('console');
+const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
@@ -15,114 +19,19 @@ const db = knex({
     }
   });
 
-// db.select('*').from('users').then(data => {
-//     console.log(data);
-// })
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// const database = {
-//     users: [
-//         {
-//             id: '123',
-//             name: "john",
-//             email: "john@gmail.com",
-//             password: "cookies",
-//             entries: 0,
-//             joining: new Date()
-//         },
-//         {
-//             id: '124',
-//             name: "sally",
-//             email: "sally@gmail.com",
-//             password: "bananas",
-//             entries: 0,
-//             joining: new Date()
-//         }
-//     ],
-//     login: [
-//         {
-//             id: '987',
-//             hash: '',
-//             email: 'john@gmail.com'
-//         }
-//     ]
-// }
-// console.log(database.users[2])
-app.get('/', (req, res) => {    
-    res.send(database.users);
-})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
-app.post('/signin', (req, res) => {
-    if (req.body.email == database.users[0].email && 
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
-    } else (res.status(400).json('error logging in'))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.post('/register', (req, res) => {
-    const { name, email, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                        name: name,
-                        email: loginEmail[0].email,
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        res.json(user[0]);
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json("unable to register"))
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfiles(req, res, db) })
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({id})
-        .then(user => {
-            console.log(user);
-            if(user.length) {
-                res.json(user);
-            }   else (
-                res.status(400).json("User not found")
-            )
-        })
-        .catch(err => res.status(400).json('error getting user'))
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0].entries);
-        })
-        .catch(err => res.status(400).json("unable to get entries"))
-})
-
-
-
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
+app.post('/imageurl', (req, res) => { image.handleApiReq(req, res) })
 
 app.listen(8080, () => {
     console.log('app is running on port 8080')
